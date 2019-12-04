81cb62f9d24d35cdc46c81858477423a09abe007
Siraj Hassan <sirajhassan@yahoo.com>
2019-12-03 21:58:38 -0600

    flask working

--- app.py
+++ app.py
@@ -9,22 +9,6 @@ import re
 import json
 
 
-
-class WordForm(FlaskForm):
-
-    avail_letters = StringField("Letters", validators= [
-        Regexp(r'^[a-z]*$', message="must contain letters only")
-    ])
-
-    pattern = StringField("Pattern", validators= [
-        Regexp(r'^[a-z.]*$', message="must contain letters or periods (.)")
-    ])
-
-    wordLength = SelectField('Length of word', choices=[("Default","Select"),("3", "3"), ("4", "4"),("5", "5"),("6", "6"),("7", "7"),("8", "8"),("9", "9"),("10","10")])
-    submit = SubmitField("Go")
-
-
-
 csrf = CSRFProtect()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "row the boat"
@@ -32,5 +16,4 @@ csrf.init_app(app)
 
 @app.route('/index')
 def index():
-    form = WordForm()
-    return render_template("index.html", form=form)
+    return render_template("index.html")
