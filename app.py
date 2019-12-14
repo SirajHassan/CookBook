c14f7ae4f9ac76a2f26aa908b6098cfa133d6cdf
Siraj Hassan <sirajhassan@yahoo.com>
2019-12-14 00:28:53 -0600

    took out all models except user

--- app.py
+++ app.py
@@ -36,29 +36,29 @@ login_manager.init_app(app)
 #SQL stuff ########################################
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///Users/sirajhassan/Desktop/webDev/CookBook/test.db'
-db = SQLAlchemy(app);
+db = SQLAlchemy(app)
 
 #Tables for db
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(30),unique = True)
-    password = db.Column(db.String(30),unique = True)
-    family_id = db.Column(db.Integer, db.ForeignKey('family.id'))
-    recipes = db.relationship('Recipe',backref='creator')
+    #password = db.Column(db.String(30),unique = True)
+    # family_id = db.Column(db.Integer, db.ForeignKey('family.id'))
+    # recipes = db.relationship('Recipe',backref='creator')
 
-class Recipe(db.Model):
-    family_id = db.Column(db.Integer, db.ForeignKey('family.id'))
-    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    name = db.Column(db.String(100),unique = True)
-    recipe = db.Column(db.String(1000),unique = True)
-    image_link = db.Column(db.String(200),unique = True)
-    time_made = db.Column(db.Integer, primary_key = True)
-
-class Family(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(30),unique = True)
-    users = db.relationship('User',backref='family')
-    recipes = db.relationship('Recipe',backref='family')
+# class Recipe(db.Model):
+#     family_id = db.Column(db.Integer, db.ForeignKey('family.id'))
+#     creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     name = db.Column(db.String(100),unique = True)
+#     recipe = db.Column(db.String(1000),unique = True)
+#     image_link = db.Column(db.String(200),unique = True)
+#     time_made = db.Column(db.Integer, primary_key = True)
+#
+# class Family(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     name = db.Column(db.String(30),unique = True)
+#     users = db.relationship('User',backref='family')
+#     recipes = db.relationship('Recipe',backref='family')
 
 
 
