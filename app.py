a2b952cc1ed512026c4c203643037d8daa7ce0a8
Siraj Hassan <sirajhassan@yahoo.com>
2019-12-15 13:08:19 -0600

    #5 login and logout work, but logout button has error..

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_wtf import Form
 from wtforms import StringField,PasswordField,BooleanField,SubmitField, SelectField
 from wtforms.validators import Regexp, InputRequired,Email,Length, AnyOf
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager,UserMixin,login_user, logout_user, current_user
+from flask_login import LoginManager,UserMixin,login_user, logout_user, current_user, login_required
 from flask_bootstrap import Bootstrap
 import re
 import json
@@ -28,10 +28,11 @@ boostrap = Bootstrap(app)
 nav = Nav()
 nav.init_app(app)
 
-
 #login stuff
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 
 #SQL stuff ########################################
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -81,6 +82,7 @@ def mynavbar():
         View('Dinner','dinner'),
         View('Dessert','dessert'),
         View('Snacks','snacks'),
+        View('Logout','logout'),
     )
 
 
@@ -88,16 +90,18 @@ def mynavbar():
 #################login stuff ###############################
 
 
+
+
 class LoginForm(FlaskForm):
     username = StringField('Username',validators = [InputRequired(), Length(min = 4, max = 30)])
     family_pin = PasswordField('Family Pin Number', validators = [InputRequired(), Length(min = 4, max = 4)])
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username',validators = [InputRequired()])
+    username = StringField('Username',validators = [InputRequired(),Length(min = 4, max = 30)])
     #password = PasswordField('Password', validators = [InputRequired()])
     new_cook_book = BooleanField('New CookBook')
     family_name = StringField('Family Cook Book Name',validators = [InputRequired(), Length(min = 4, max = 40)])
-    family_pin = PasswordField('Family Pin Number',validators = [InputRequired(), Length(min = 4, max = 4)])
+    family_pin = PasswordField('Family Pin Number (4 values)',validators = [InputRequired(), Length(min = 4, max = 4)])
 
 
 #function that flask login uses to connect abstract user
@@ -110,15 +114,51 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return render_template("login.html")
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
 
+    if current_user.is_authenticated:
+        return render_template("dashboard.html")
+
 
     if form.validate_on_submit(): #if form has been submitted properly
-        return 'login is good'
+        user_exists = db.session.query(User.username).filter_by(username=form.username.data).scalar() is not None
+
+        # if user exists check if pin matches..
+        if user_exists == True:
+            user = User.query.filter_by(username=form.username.data).first()
+            family_id = user.family_id
+            family = Family.query.get(family_id)
+            pin = family.pin
+            #return(str(pin)+ ' ' + str(form.family_pin.data))
+
+            if str(pin) == str(form.family_pin.data): #pin matches
+                login_user(user)
+                return render_template("dashboard.html")
+            else:
+                flash('Error, Pin is not correct')
+                return render_template("login.html", form = form)
+
+
+        else:
+            flash('Error Username Does not exist try a different name')
+            return render_template("login.html", form = form)
+
 
     return render_template("login.html", form = form)
+
+
+
+
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -145,12 +185,14 @@ def signup():
                 flash('Error Username Exists try a different name')
                 return render_template("signup.html", form = form)
 
-            else:
+            else: #add user to database go to login
                 db.session.add(family)
                 db.session.add(user)#order of family and user might be issue?
                 db.session.commit()
+                flash('Username: ' + str(form.username.data) + 'created successfully')
+                return render_template("login.html")
 
-            return('user added')
+
 
         else:
 
@@ -161,7 +203,7 @@ def signup():
             #check if pin matches family
             if(family_exists):
                 # check if pin is correct
-                fam = db.session.query(Family.name).filter_by(pin= form.family_pin.data, name = form.family_name.data).all() #better way to do this
+                fam = db.session.query(Family.name).filter_by(pin= form.family_pin.data, name = form.family_name.data).all() #better way to do this?
                 if fam != []: # pin and name exist as combo
                     user = User(username = form.username.data)
                     family = Family.query.filter_by(name=form.family_name.data).first()
@@ -195,42 +237,33 @@ def index():
 
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    return render_template("dashboard.html")
+    return render_template("dashboard.html", name=current_user.username)
 
 ##### meals ######
 
 @app.route('/breakfast')
+@login_required
 def breakfast():
     return render_template("breakfast.html")
 
 @app.route('/lunch')
+@login_required
 def lunch():
     return render_template("lunch.html")
 
 @app.route('/dinner')
+@login_required
 def dinner():
     return render_template("dinner.html")
 
 @app.route('/dessert')
+@login_required
 def dessert():
     return render_template("dessert.html")
 
 @app.route('/snacks')
+@login_required
 def snacks():
     return render_template("snacks.html")
-
-
-
-
-
-################# other functions  ####################
-
-#add user to the db
-def create_user(name,id,password,family,db):
-    print('creating user')
-
-# get all data from recipes and generate to html
-# def GenerateRecipes(fam_id):
-#     family_recipes = Recipe.query.filter_by(family_id = fam_id)
-#     return family_recipes
